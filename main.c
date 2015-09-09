dd4b787accad5d807a36f0ec2d01830758865d83
TretUliy2 <tretuliy2@gmail.com>
2015-09-09 08:44:28 +0300

    drop_mgroup added to exit_nice() in main.c

--- main.c
+++ main.c
@@ -239,7 +239,11 @@ int main(int argc, char **argv) {
 
 void exit_nice(void) {
 	int err, i;
-
+	for ( i = 0; i < srv_count; i++ ) {
+		if ( server_cfg[i].streaming == 1 ) {
+			drop_mgroup(i);
+		}
+	}
 	for (i = 0; i < thr; i++) {
 		err = pthread_cancel(threads[i]);
 		if (err != 0) {
